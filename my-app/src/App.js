660b6b8952fab9de7afb71d5b26f5b1ed09e04ef
OlgaGre <grechanyuko@gmail.com>
2022-08-02 20:26:34 +0300

    arr

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-function App() {
-	return <div>
-    <p className='dghg'>gsgseg</p>
-    <p className='sdgsdg'>gggseg</p>
-		text
-    yudtiutdu
-    <p>fegwegweg</p>
-    <input />
-  <ul><li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li></ul>
-    <input />
-	</div>;
-}
+const users = [
+	{id: 1, name: 'user1', surn: 'surn1', 
+		age: 30}, 
+	{id: 2, name: 'user2', surn: 'surn2', 
+		age: 31}, 
+	{id: 3, name: 'user3', surn: 'surn3', 
+		age: 32}, 
+];
 
+function App() {
+	const res = users.map(function(item) {
+		return <tr key={item.id}>
+      <td>{item.name}</td>
+      <td>{item.surn}</td>
+    </tr>
+	});
+	return <table>
+    <tbody>
+      {res}
+    </tbody>
+  </table>
+}
 export default App;
