0a037e70a84d2a8e08079abecd1c1cb9e216d79e
OlgaGre <grechanyuko@gmail.com>
2022-08-02 17:21:26 +0300

    JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function App() {
 	return <div>
+    <p className='dghg'>gsgseg</p>
+    <p className='sdgsdg'>gggseg</p>
 		text
+    yudtiutdu
+    <p>fegwegweg</p>
+    <input />
+  <ul><li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li></ul>
+    <input />
 	</div>;
 }
 
