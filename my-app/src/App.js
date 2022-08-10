f01387b190c51b62aff7d5074d92c574eaff4fc9
OlgaGre <grechanyuko@gmail.com>
2022-08-10 15:41:15 +0300

    work with input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,21 @@
 
 import React, { useState } from "react";
 
+
 function App() {
-  let [name, changeName] = useState("olga");
-  let [sname, changeSname] = useState("gre");
-  let [age, changeAge] = useState(18);
-  let [ban, changeBan] = useState(false);
-  return (
-    <div>
-      <span>{name}</span>
-      <button onClick={() => changeName("OLGA")}>change name</button>
-      <br></br>
-      <span>
-        {sname}
-        <button onClick={() => changeSname("GRE")}>change sname</button>
-      </span>
-			<br></br>
-      <span>{age}</span>
-      <button onClick={()=> changeAge(age ++)}>+</button>
-      <button onClick={()=> changeAge(age --)}>-</button>
-      <div>{ban ? "ban" : "no ban"}</div>
-      {!ban && <button onClick={() => changeBan(true)}>ban</button>}
-      {ban && <button onClick={() => changeBan(false)}>delete ban</button>}
-    </div>
-  );
+	const [value1, setValue1] = useState('');
+	const [value2, setValue2] = useState('');
+	const [result1, setResult1] = useState('');
+	const [result2, setResult2] = useState('');
+	
+	return <div>
+		<input value={value1} onChange={event => setValue1(event.target.value)} />
+		<input value={value2} onChange={event => setValue2(event.target.value)} />
+<button onClick={() => setResult1(Number(value1) + Number(value2))}>+</button>
+<button onClick={() => setResult2(Number(value1) * Number(value2))}>*</button>
+		<p>text: {result1}</p>
+		<p>text: {result2}</p>
+	</div>;
 }
 
 export default App;
