48e67c4356ca29a100b5373273317e25073ed63a
OlgaGre <grechanyuko@gmail.com>
2022-10-20 23:17:08 +0300

    feat: useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,11 @@
-// import React from 'react';
-
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
 
 function App() {
-	const [value1, setValue1] = useState('');
-	const [value2, setValue2] = useState('');
-	const [result1, setResult1] = useState('');
-	const [result2, setResult2] = useState('');
+	let [name, setName] = useState('prod');
 	
 	return <div>
-		<input value={value1} onChange={event => setValue1(event.target.value)} />
-		<input value={value2} onChange={event => setValue2(event.target.value)} />
-<button onClick={() => setResult1(Number(value1) + Number(value2))}>+</button>
-<button onClick={() => setResult2(Number(value1) * Number(value2))}>*</button>
-		<p>text: {result1}</p>
-		<p>text: {result2}</p>
+		<span>{name}</span>
+		<button onClick={() => setName('xxxx')}>btn</button>
 	</div>;
 }
 
